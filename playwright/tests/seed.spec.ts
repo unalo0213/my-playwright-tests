ee54870455badb51bf7babf864a8bbdc2ffa659e
unalo0213 <una.lo@car-plus.com.tw>
2025-12-27 18:37:33 +0800

    修改seed

--- playwright/tests/seed.spec.ts
+++ playwright/tests/seed.spec.ts
@@ -2,23 +2,20 @@ import { test } from "./base/webAuth";
 import { getConfig } from "./web/web.config";
 const config = getConfig();
 
-test.describe("官網_中古車預約Seed)", () => {
-  //需要keep登入的，就用authPage，不用的就用page即可
-  test("PRE_FE+BE", async ({ authPage }) => {
-    await authPage.goto(`${process.env.BASE_URL}/preowned/list`);
-    await authPage
-      .locator("div")
-      .filter({ hasText: /^中和營業所$/ })
-      .nth(2)
-      .click();
-    await authPage.getByText("預約賞車").nth(1).click();
-    await authPage.fill('input[name="account"]', process.env.ACCOUNT);
-    await authPage.click("#formLoginSubmit");
-    await authPage.fill('input[name="password"]', process.env.PASSWORD);
-    // 避免登入太快
-    await authPage.waitForTimeout(800 + Math.random() * 1200);
-    await authPage.locator("button").filter({ hasText: "下一步" }).click();
-    await authPage.getByRole("button", { name: "下一步" }).click();
-    await authPage.getByRole("button", { name: "確定" }).click();
-  });
+test("官網_中古車預約Seed PRE_FE+BE", async ({ authPage }) => {
+  await authPage.goto(`${process.env.BASE_URL}/preowned/list`);
+  await authPage
+    .locator("div")
+    .filter({ hasText: /^中和營業所$/ })
+    .nth(2)
+    .click();
+  await authPage.getByText("預約賞車").nth(1).click();
+  await authPage.fill('input[name="account"]', process.env.ACCOUNT);
+  await authPage.click("#formLoginSubmit");
+  await authPage.fill('input[name="password"]', process.env.PASSWORD);
+  // 避免登入太快
+  await authPage.waitForTimeout(800 + Math.random() * 1200);
+  await authPage.locator("button").filter({ hasText: "下一步" }).click();
+  await authPage.getByRole("button", { name: "下一步" }).click();
+  await authPage.getByRole("button", { name: "確定" }).click();
 });
